36056258f0cb5c175d933e55330e6435ccbb839e
Pavan Gole <pavangoley5@gmail.com>
2023-11-07 19:15:06 +0530

    Tried Using Priority Queue

--- src/leetcodedaily/EliminateMonster.java
+++ src/leetcodedaily/EliminateMonster.java
@@ -1,6 +1,7 @@
 package leetcodedaily;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class EliminateMonster {
     public static void main(String[] args) {
@@ -22,5 +23,35 @@ public class EliminateMonster {
 
         }
         return i;
+    }
+
+    static int eliminateMaximumusingPQ(int[] dist, int[] speed) {
+        PriorityQueue<Integer> time = new PriorityQueue<>();
+        for (int i = 0; i < dist.length; i++) {
+            time.add((int) Math.ceil((double) dist[i] / speed[i]));
+        }
+        int i;
+        for( i = 0; i < time.size(); i++) {
+            if(time.peek() - i == 0) {
+                break;
+            }
+            time.poll();
+        }
+        return i;
+        // int[] time = new int[dist.length];
+        
+        // for (int i = 0; i < dist.length; i++) {
+        //     time[i] = (int) Math.ceil((double) dist[i] / speed[i]);
+        // }
+        // Arrays.sort(time);
+        // int i;
+        // for( i = 0; i < time.length; i++) {
+        //     if(time[i] - i == 0) {
+        //         break;
+        //     }
+
+        // }
+        // return i;
+        
     }
 }
